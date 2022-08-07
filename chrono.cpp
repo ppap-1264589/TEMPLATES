88fe54cda8b19d9ded1d66dcc1b89b056317fe8d
ppap-1264589 <88699088+ppap-1264589@users.noreply.github.com>
2022-08-07 17:10:51 +0700

    Update chrono.cpp

--- chrono.cpp
+++ chrono.cpp
@@ -1,5 +1,5 @@
-mt19937 RNG(chrono::high_resolution_clock::now().time_since_epoch().count());
-int UID(int l, int r){
-    uniform_int_distribution<mt19937::result_type> random_number(l, r);
+mt19937_64 RNG(chrono::high_resolution_clock::now().time_since_epoch().count());
+long long UID(long long l, long long r){
+    uniform_int_distribution<mt19937_64::result_type> random_number(l, r);
     return random_number(RNG);
 }
