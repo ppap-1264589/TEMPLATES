501ad3588b9de28c098f405860ef7ca04af60f5f
ppap-1264589 <88699088+ppap-1264589@users.noreply.github.com>
2022-11-10 22:34:58 +0700

    Delete chrono.cpp

--- chrono.cpp
+++
@@ -1,5 +0,0 @@
-mt19937_64 RNG(chrono::high_resolution_clock::now().time_since_epoch().count());
-long long UID(long long l, long long r){
-    uniform_int_distribution<mt19937_64::result_type> random_number(l, r);
-    return random_number(RNG);
-}
